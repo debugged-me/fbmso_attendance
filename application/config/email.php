df0c52282b1cbcb84fd5018d2bcd0cd15677eeed
Clark Steven <edongclarksteven@gmail.com>
2026-08-20 08:15:16 +0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,13 +3,13 @@
 
 
 $config['protocol']     = 'smtp';
-$config['smtp_host']    = 'mail.softtechco.biz';
-$config['smtp_user']    = 'fbmso@softtechco.biz';
+$config['smtp_host']    = 'fbmso.srmsportal.com';
+$config['smtp_user']    = 'attendance@fbmso.srmsportal.com';
 $config['smtp_pass']    = getenv('FBMSO_SMTP_PASSWORD') ?: 'moth34board';
 $config['smtp_port']    = 465;
 $config['smtp_crypto']  = 'ssl';
 
-$config['smtp_timeout'] = 5;
+$config['smtp_timeout'] = 10;
 $config['mailtype']     = 'html';
 $config['charset']      = 'utf-8';
 $config['newline']      = "\r\n";
